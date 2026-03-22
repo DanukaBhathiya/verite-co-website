f959fcd507033215f96ab2389a968164b229a7a9
PM_User <danuka@paymedia.lk>
2026-03-22 13:29:00 +0530

    ready to deploy to vercel - 3

--- src/components/Collections.js
+++ src/components/Collections.js
@@ -15,13 +15,14 @@ function Collections() {
 
   const settings = {
     dots: true,
+    arrows: true,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [
       { breakpoint: 1024, settings: { slidesToShow: 2 } },
-      { breakpoint: 768, settings: { slidesToShow: 1 } }
+      { breakpoint: 768, settings: { slidesToShow: 1, arrows: false } }
     ]
   };
 
